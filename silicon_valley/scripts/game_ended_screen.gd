5edfa739c443fd37b1e3657a8887162fd6fa65f6
xShader1374 <shader1374@gmail.com>
2025-04-09 02:23:25 +0200

    GameEndedScreen Added

---
+++ silicon_valley/scripts/game_ended_screen.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func showAnimation() -> void:
+	%VBoxContainer.pivot_offset = %VBoxContainer.size / 2.0
+	
+	var tween: Tween = create_tween()
+	
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_EXPO)
+	
+	tween.tween_property(%VBoxContainer, "scale:y", 1.0, 0.5).from(0.0)
+	tween.parallel().tween_property(%VBoxContainer, "scale:x", 1.0, 0.5).from(0.0).set_delay(0.075)
+
+
+func _on_v_box_container_resized() -> void:
+	%VBoxContainer.pivot_offset = %VBoxContainer.size / 2.0
+
+
+func _on_debug_timer_timeout() -> void:
+	showAnimation()
