e3f772e99f3de4fdc9a14d35ff36caed6d6b34de
xShader1374 <shader1374@gmail.com>
2025-04-10 02:33:09 +0200

    Sky Update

--- silicon_valley/scripts/sponsor_node_2d.gd
+++ silicon_valley/scripts/sponsor_node_2d.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "show_sponsors":
-		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+		get_tree().change_scene_to_file("uid://dblo1acmeuiyl") # "res://scenes/Main_Menu/main_menu.tscn", main menu scene
