28a27235f44eb1b7a1db4d5731af683ccb0042a4
xShader1374 <shader1374@gmail.com>
2025-04-10 16:18:27 +0200

    Update 2 big

--- silicon_valley/Autoloads/Ambience_Player/ambience_player.gd
+++ silicon_valley/Autoloads/Ambience_Player/ambience_player.gd
@@ -1,11 +1,50 @@
-extends Node
+extends AudioStreamPlayer
 
+## Ambience will still play even if paused, to change this behavior,
+## you can change the process mode from "always" to "inherit".
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const Ambiences: Array[AudioStream] = [
+	
+]
 
+func transitionFromCurrentToNew(new_stream: AudioStream, total_duration: float) -> void:
+	var tween1: Tween = create_tween()
+	
+	tween1.set_ease(Tween.EASE_IN_OUT)
+	tween1.set_trans(Tween.TRANS_LINEAR)
+	
+	# If stream isn't empty
+	if stream:
+		# Fade out
+		tween1.tween_property(self, "volume_db", -80.0, total_duration / 2.0).from_current()
+		
+		# Cambio stream e riavvio
+		tween1.chain().tween_callback(func() -> void: 
+			set_stream(new_stream)
+			play())
+		
+		# Fade in
+		tween1.chain().tween_property(self, "volume_db", 0.0, total_duration / 2.0).from(-80.0)
+	else:
+		tween1.tween_callback(func() -> void: 
+			set_stream(new_stream)
+			play())
+		
+		# Fade in
+		tween1.chain().tween_property(self, "volume_db", 0.0, total_duration).from(-80.0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func changeAmbience(ambience_ID: int, trans_duration: float = 0.15, forced_override: bool = true) -> void:
+	if ambience_ID >= 0 and ambience_ID < Ambiences.size():
+		if forced_override: # If forced_override ON (overrides stream even if same as current stream)
+			transitionFromCurrentToNew(Ambiences[ambience_ID], trans_duration)
+		elif Ambiences[ambience_ID] != stream: # If forced_override OFF && current stream isn't equal to the new
+			transitionFromCurrentToNew(Ambiences[ambience_ID], trans_duration)
+
+func quitAmbience() -> void:
+	stop()
+	stream = null
+
+## TODO: WIP
+func quitAmbienceSmooth() -> void:
+	stop()
+	stream = null
--- silicon_valley/Autoloads/Ost_Player/ost_player.gd
+++ silicon_valley/Autoloads/Ost_Player/ost_player.gd
@@ -1,11 +1,45 @@
-extends Node
+extends AudioStreamPlayer
 
+const OSTS: Array[AudioStreamOggVorbis] = [
+	
+]
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func transitionFromCurrentToNew(new_stream: AudioStreamOggVorbis, total_duration: float) -> void:
+	var tween1: Tween = create_tween()
+	
+	tween1.set_ease(Tween.EASE_IN_OUT)
+	tween1.set_trans(Tween.TRANS_LINEAR)
+	
+	# Se stream non è vuoto
+	if stream:
+		# Fade out
+		tween1.tween_property(self, "volume_db", -80.0, total_duration / 2.0).from_current()
+		# Cambio stream e riavvio
+		tween1.chain().tween_callback(func() -> void: 
+			set_stream(new_stream)
+			play())
+		# Fade in
+		tween1.chain().tween_property(self, "volume_db", 0.0, total_duration / 2.0)
+	else:
+		volume_db = -80.0
+		tween1.tween_callback(func() -> void: 
+			set_stream(new_stream)
+			play())
+		# Fade in
+		tween1.chain().tween_property(self, "volume_db", 0.0, total_duration)
 
+func changeMusic(music_ID: int, trans_duration: float = 0.15, forced_override: bool = true) -> void:
+	if music_ID >= 0 and music_ID < OSTS.size():
+		if forced_override: # If forced_override ON (overrides stream even if same as current stream)
+			transitionFromCurrentToNew(OSTS[music_ID], trans_duration)
+		elif OSTS[music_ID] != stream: # If forced_override OFF && current stream isn't equal to the new
+			transitionFromCurrentToNew(OSTS[music_ID], trans_duration)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func quitMusic() -> void:
+	stop()
+	stream = null
+
+## TODO: WIP
+func quitMusicSmooth() -> void:
+	stop()
+	stream = null
